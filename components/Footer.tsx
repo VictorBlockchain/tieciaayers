6c087be05715bc28fc7a7a22a2091a6bd2d0bb8d
VictorBlockchain <93537143+VictorBlockchain@users.noreply.github.com>
2024-11-05 10:47:59 -0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <div className="cs-height_100 cs-height_lg_60"></div>
             <div className="container">
                 <div className="text-center">
-                    <p className="cs-page_title" style={{fontFamily: 'Comfortaa'}}><small>Not A Financial instrument <br/>Not financial advice<br/></small> </p>
+                    {/* <p className="cs-page_title" style={{fontFamily: 'Comfortaa'}}><small>Link to complaint with Texas Bar Ass</small> </p> */}
                 </div>
             </div>
             <div className="cs-height_60 cs-height_lg_20"></div>
@@ -18,7 +18,7 @@ const Footer = () => {
             <div className="container">
             <div className="cs-footer_separetor"></div>
             <div className="text-center">
-            <div className="text-center p-3">Copyright 2024 GirlWif.meme</div>
+            <div className="text-center p-3">Copyright 2024 TieciaAyers.com</div>
             <ul className="cs-footer_menu">
             {/* <li><a href="/privacy">Privacy Policy</a></li>
             <li><a href="/terms">Term &amp; Condition</a></li> */}
